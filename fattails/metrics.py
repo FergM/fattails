99441191554b6767bafb31d34101ea8cc73d2833
FergM <45894043+FergM@users.noreply.github.com>
2021-01-30 21:15:20 +0000

    Add survival probability method

--- fattails/metrics.py
+++ fattails/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def mad(x):
     """Calculate Mean Absolute Deviation
@@ -25,3 +26,46 @@ def mad(x):
     mad = np.mean(absolute_deviation)
 
     return mad
+
+def get_survival_probability(arr):
+    """Calculate sample probability of X >= x, for each value in `arr`.
+
+    Duplicate values are treated as ascending values based on
+    their position in `arr`.
+
+    Parameters
+    ----------
+    arr : array_like
+        Numeric values on the real number line
+
+    Returns
+    -------
+    survival_probability_sr : Pandas Series
+    """
+
+    sr = pd.Series(arr, name='input_data') # Convert input to pandas series
+    sr = sr.sort_values()                  # Sort from low to high
+
+    # Reset index
+    df = sr.reset_index()   # Keeps the old index
+    df = df.rename(columns={'index':'input_index'})
+
+    # Label relative positions
+    gap_count = len(sr) + 1           # Think of the Posts and Fences analogy
+    df['left_gap_count'] = df.index+1                     # Count values <= x
+    df['right_gap_count'] = gap_count - df.left_gap_count # Count values >= x
+
+    # Get survival Probability
+    df['survival_probability'] = df.right_gap_count / gap_count
+
+    #===================================================
+    #Format the Output
+
+    #Reset original Order
+    df = df.sort_values(by='input_index')
+
+    #Extract the output series
+    survival_probability_sr = df.survival_probability
+    survival_probability_sr.reindex(df.input_index)
+
+    return survival_probability_sr
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,28 @@
 import numpy as np
+import pandas as pd
+import pytest
 
-from fattails.metrics import mad
+
+from fattails.metrics import mad, get_survival_probability
+
+@pytest.fixture
+def survival_data_examples():
+    """Example input and output to test get_survival_probability"""
+    two_equal_values = {}
+    two_equal_values['input'] = [2,2,3]
+    two_equal_values['output'] = [0.75, 0.5, 0.25]
+
+    negative_and_positive = {}
+    negative_and_positive['input'] = [-1,-0.3,7]
+    negative_and_positive['output'] = [0.75, 0.5, 0.25]
+
+    not_monotonic = {}
+    not_monotonic['input'] = [2,3,2]
+    not_monotonic['output'] = [0.75, 0.25, 0.5]
+
+    examples = [two_equal_values, not_monotonic, negative_and_positive]
+
+    return examples
 
 class TestMad:
     """Test the mean absolute deviation method"""
@@ -22,3 +44,16 @@ class TestMad:
         mad_ = mad(x)
 
         assert mad_ == 0
+
+class TestGetSurvivalProbability:
+
+    def test_returns_expected_results(self, survival_data_examples):
+
+        for example in survival_data_examples:
+            data = example['input']
+            expected = example['output']
+
+            output = get_survival_probability(data)
+
+            assert output.name == 'survival_probability'
+            assert output.to_list() ==  example['output']
