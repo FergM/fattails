b54ca6081061315d6cbe2c9e66d91db3e9124777
FergM <45894043+FergM@users.noreply.github.com>
2021-01-31 10:59:38 +0000

    Update test to use parametrizations

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,25 +5,6 @@ import pytest
 
 from fattails.metrics import mad, get_survival_probability
 
-@pytest.fixture
-def survival_data_examples():
-    """Example input and output to test get_survival_probability"""
-    two_equal_values = {}
-    two_equal_values['input'] = [2,2,3]
-    two_equal_values['output'] = [0.75, 0.5, 0.25]
-
-    negative_and_positive = {}
-    negative_and_positive['input'] = [-1,-0.3,7]
-    negative_and_positive['output'] = [0.75, 0.5, 0.25]
-
-    not_monotonic = {}
-    not_monotonic['input'] = [2,3,2]
-    not_monotonic['output'] = [0.75, 0.25, 0.5]
-
-    examples = [two_equal_values, not_monotonic, negative_and_positive]
-
-    return examples
-
 class TestMad:
     """Test the mean absolute deviation method"""
 
@@ -45,15 +26,16 @@ class TestMad:
 
         assert mad_ == 0
 
+@pytest.mark.parametrize("description, input_data, expected_output", [
+    ("duplicate_values",  [ 2,   2, 3], [0.75, 0.5,  0.25]),
+    ("negative_values",   [-1,-0.3, 7], [0.75, 0.5,  0.25]),
+    ("not_sorted_values", [ 2,   3, 2], [0.75, 0.25,  0.5]),
+])
 class TestGetSurvivalProbability:
 
-    def test_returns_expected_results(self, survival_data_examples):
+    def test_returns_expected_results(self, description, input_data, expected_output):
 
-        for example in survival_data_examples:
-            data = example['input']
-            expected = example['output']
+        output = get_survival_probability(input_data)
 
-            output = get_survival_probability(data)
-
-            assert output.name == 'survival_probability'
-            assert output.to_list() ==  example['output']
+        assert output.name == 'survival_probability'
+        assert output.to_list() ==  expected_output
