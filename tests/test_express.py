0e808372ee11d9566f2ac2b98f28d35ac2b7ecc7
FergM <45894043+FergM@users.noreply.github.com>
2021-02-27 01:37:27 +0000

    Add test for plot_MS_moments

--- tests/test_express.py
+++ tests/test_express.py
@@ -1,4 +1,6 @@
 from fattails import express
+from matplotlib.axes._subplots import SubplotBase
+import pandas as pd
 import pytest
 
 
@@ -6,7 +8,8 @@ class TestImports:
     """ Check that no imports are missing.
     """
 
-    def test_mad_method(self):
+    def test_mad_method_exists(self):
+        """Check mad exists as expected in this namespace"""
 
         # Check that the attribute exists
         try:
@@ -15,3 +18,18 @@ class TestImports:
             # Fail test if `express` doesn't have a `mad` attribute
             msg = 'The method called `mad` is missing from the express module.'
             pytest.fail(msg)
+
+
+class TestPlotMSMoments:
+
+    @pytest.mark.parametrize("description, input_data", [
+                            ("list_data",  [1, 2, 3]),
+                            ("series_data",  pd.Series([1, 2, 3])),
+                            ])
+    def testplot_MS_moments(self, description, input_data):
+
+        axes = express.plot_MS_moments(input_data)
+        first_axis = axes[0, 0]
+
+        assert axes.shape == (2, 2)
+        assert isinstance(first_axis, SubplotBase)
