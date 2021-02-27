91777fcde2cbe08ff0931819ccd9a806f3ac0417
FergM <45894043+FergM@users.noreply.github.com>
2021-02-27 16:22:46 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fattails", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="FergM",
     description="A package for fat-tailed statistics",
     long_description=long_description,
