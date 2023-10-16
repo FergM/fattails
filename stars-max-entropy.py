1e029a8182e2e15d35ba2c74a92d3de5ef27a849
Ronan McGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-10-16 23:02:56 +0100

    Update stars-max-entropy.py

--- stars-max-entropy.py
+++ stars-max-entropy.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import numpy as np
+
 # Define Shannon entropy for Multinomial distribution with a fixed number of samples
 def shannon_entropy_multinomial_fixed_samples(p, n):
     entropy = -np.sum(p_i * np.log2(p_i) for p_i in p if p_i > 0)  # Exclude zero terms
@@ -8,25 +12,50 @@ def constraint_average_fixed_samples(p, avg, n):
     weighted_sum = np.sum(i * p_i for i, p_i in enumerate(p, start=1))
     return n * weighted_sum - n * avg  # Account for the number of samples
 
-# Known average review and number of reviews
-# Takes in the average rating and number of stars and outputs the p distribution given max entropy
+# Function to perform optimization for given number of reviews and average rating
+def optimize_probs(num_reviews, avg_review):
+    # Initial guess for p
+    initial_p = [0.2, 0.2, 0.2, 0.2, 0.2]
+    
+    # Constraint for average review
+    con_avg = {'type': 'eq', 'fun': constraint_average_fixed_samples, 'args': (avg_review, num_reviews)}
+    
+    # Additional constraint for probabilities to sum to 1
+    con_sum = {'type': 'eq', 'fun': lambda p: np.sum(p) - 1}
+    
+    # Perform optimization to maximize entropy
+    result = minimize(shannon_entropy_multinomial_fixed_samples, initial_p, args=(num_reviews,), constraints=[con_avg, con_sum], bounds=[(0, 1)]*5)
+    
+    # Extract optimized p values
+    return result.x
 
-avg_review = 3.5
-num_reviews = 10
+# Known average review
+avg_review = 4.0
 
-# Initial guess for p
-initial_p = [0.2, 0.2, 0.2, 0.2, 0.2]
+# Different number of reviews
+num_reviews_list = [4, 8, 16, float('inf')]
 
-# Constraint for average review
-con_avg = {'type': 'eq', 'fun': constraint_average_fixed_samples, 'args': (avg_review, num_reviews)}
+# Store optimized probabilities
+optimized_probs = {}
 
-# Additional constraint for probabilities to sum to 1
-con_sum = {'type': 'eq', 'fun': lambda p: np.sum(p) - 1}
+# Perform optimization for each number of reviews
+for num_reviews in num_reviews_list:
+    optimized_p = optimize_probs(num_reviews, avg_review) if num_reviews != float('inf') else optimize_probs(10000, avg_review)  # Use a large number to approximate infinity
+    optimized_probs[num_reviews] = optimized_p
 
-# Perform optimization to maximize entropy
-result_fixed_samples = minimize(shannon_entropy_multinomial_fixed_samples, initial_p, args=(num_reviews,), constraints=[con_avg, con_sum], bounds=[(0, 1)]*5)
+# Adjusted plot to show a dot plot for each case
+fig, ax = plt.subplots()
 
-# Extract optimized p values
-optimized_p_multinomial_fixed_samples = result_fixed_samples.x
+# Different markers for better visibility
+markers = ['o', 's', '^', 'x']
 
-optimized_p_multinomial_fixed_samples
+for i, (num_reviews, optimized_p) in enumerate(optimized_probs.items()):
+    ax.plot(range(1, 6), optimized_p, marker=markers[i], linestyle='-', label=f'{num_reviews} Reviews')
+
+# Labels and title
+ax.set_xlabel('Star Rating')
+ax.set_ylabel('Probability')
+ax.set_title('Optimized Probabilities for Different Numbers of Reviews')
+ax.legend()
+
+plt.show()
